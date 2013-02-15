24d8265c7056a4231e8874b0ec770d640226cade
naresh <naresh@ariveguru.com>
2013-02-15 12:07:22 +0530

    another commited...

--- First_EServ_Pro/src/com/ariveguru/git/SampleTest.java
+++ First_EServ_Pro/src/com/ariveguru/git/SampleTest.java
@@ -6,4 +6,20 @@ public class SampleTest {
 	static{
 		System.out.println("Welcome to GIT World..");
 	}
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public SampleTest() {
+		// TODO Auto-generated constructor stub
+		System.out.println("constructor...");
+	}
 }
