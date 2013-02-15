47c63093154e2e252a903e4c5e89fd79ccfcd722
naresh <naresh@ariveguru.com>
2013-02-15 13:07:09 +0530

    initially commited....

---
+++ SimpleMaven/src/com/web/servlet/SimpleServlet.java
@@ -0,0 +1,30 @@
+package com.web.servlet;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/helo.html")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("t1");
+		PrintWriter out=response.getWriter();
+		out.println("<html>");
+		out.println("<body><h3> Hello From Servlet</h3><br/>");
+		out.println("Name is: "+name);
+		out.println("</body>");
+		out.println("</html>");
+		System.out.println("Hello From Servlet....");
+	}
+}
